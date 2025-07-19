789971085429f435d460d13b5ea3b8bc7620759a
remus.jones <remus.jones@hotmail.com>
2025-07-19 16:59:13 +1000

    added imgui and rlimGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
+#include "imgui.h"
 #include "raylib.h"
+#include "rlImGui.h"
 
 int main()
 {
     InitWindow(800, 450, "raylib [core] example - basic window");
 
+    rlImGuiSetup(true);
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        rlImGuiBegin();
+
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        rlImGuiEnd();
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow();
-
     return 0;
 }
